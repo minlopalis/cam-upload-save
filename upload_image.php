168c900320fce7a814a093a13c5b2b6cb0c73a8f
minlopalis <52741304+minlopalis@users.noreply.github.com>
2019-12-31 08:58:31 +1300

    Create upload_image.php

---
+++ upload_image.php
@@ -0,0 +1,7 @@
+<?php 
+    if (isset($_POST["save_image"])){
+        $uri = $_POST["image_URI"];
+        file_put_contents("images/snap" . date('YmdHis') . ".png",file_get_contents($uri));
+        header("Location: declaration.php");
+    }
+?>;
