97992252fb6b22acc008a8f74d8f09c684d98774
minlopalis <52741304+minlopalis@users.noreply.github.com>
2019-12-31 09:15:13 +1300

    Update upload_image.php

--- upload_image.php
+++ upload_image.php
@@ -2,6 +2,5 @@
     if (isset($_POST["save_image"])){
         $uri = $_POST["image_URI"];
         file_put_contents("images/snap" . date('YmdHis') . ".png",file_get_contents($uri));
-        header("Location: declaration.php");
     }
 ?>;
